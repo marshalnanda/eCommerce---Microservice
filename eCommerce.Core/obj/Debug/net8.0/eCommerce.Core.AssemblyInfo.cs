9d9f251305eaf592d21496945e002639d11d6b9c
marshalnanda <mknanda678@gmail.com>
2025-09-30 00:36:19 +0530

    Additional commit

--- eCommerce.Core/obj/Debug/net8.0/eCommerce.Core.AssemblyInfo.cs
+++ eCommerce.Core/obj/Debug/net8.0/eCommerce.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("eCommerce.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6a5fa37a1a7e442aaa94ca72393d1f1cf108c27")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5505814ca48a258ff2c884ead008baea8c29c1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("eCommerce.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("eCommerce.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
